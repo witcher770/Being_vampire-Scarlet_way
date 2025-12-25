a98cdbe6859ba9b3c7180b02ea9de6cb0f229d1b
Witcher770 <maksimuryashov@gmail.com>
2025-12-26 01:38:07 +0300

    переименование всех файлов на английский

---
+++ тишина-веков/scenes/dungeon_loader.gd
@@ -0,0 +1,138 @@
+extends Node
+
+# Централизованный словарь всех загружаемых ресурсов
+const PRELOADS = {
+	# Игрок
+	"player": preload("res://сцены/player/Player.tscn"),
+	
+	# Комнаты
+	"start_room": preload("res://сцены/элементы для генерации уроней/комнаты/start_room.tscn"),
+	"boss_room_1": preload("res://сцены/элементы для генерации уроней/boss_skilet_lev1.tscn"), 
+	"boss_room_2": preload("res://сцены/boss_ricar_lev2.tscn"),
+	
+	# Уровни/генераторы
+	"generation_node": preload("res://сцены/элементы для генерации уроней/узел_генерации.tscn"),
+	
+	"end_game_scene": preload("res://сцены/интерфейс/end_game_screen.tscn")
+}
+
+@onready var player_container = $PlayerContainer
+@onready var level_container = $LevelContainer
+
+var player_instance = null
+var current_level = null
+
+
+func _ready():
+	load_player()
+	load_start_room()
+
+
+func load_player():
+	if player_instance:
+		return
+	
+	player_instance = PRELOADS.player.instantiate()
+	player_container.add_child(player_instance)
+
+	# восстановление здоровья
+	player_instance.player_health = GameState.player_health
+
+
+func load_start_room():
+	unload_level()
+
+	current_level = PRELOADS.start_room.instantiate()
+	level_container.add_child(current_level)
+	
+	# подписываемся на сигнал покидания стартовой комнаты
+	current_level.leave_start_room.connect(_on_level_finished)
+
+	move_player_to_spawn(current_level)
+
+func _on_level_finished():
+	player_container.get_child(0).heal(15)
+	GameState.num_floor = GameState.num_floor + 1
+	
+	# ЕСЛИ ИГРА ПРОЙДЕНА — ИДЁМ В ФИНАЛ
+	if GameState.game_completed and GameState.game_mode == GameState.GameMode.STORY:
+		# если победили последнего босса и игра еще не в бесконечном режиме
+		load_end_game_scene()
+		return
+		
+	#print(GameState.num_floor)
+	if GameState.num_floor_for_boss == GameState.levels_for_boss:
+		load_boss_room()
+		return
+	# при получении сигнала на вхождение в дверь - переход на следующий уровень
+	load_generated_level()
+
+func load_end_game_scene():
+	unload_level()
+	
+	GameState.num_global_level -= 1
+	
+	current_level = PRELOADS.end_game_scene.instantiate()
+	level_container.add_child(current_level)
+	move_player_to_spawn(current_level)
+	
+	current_level.continue_game.connect(_on_level_finished)
+
+
+func load_generated_level():
+	unload_level()
+	GameState._enemies_count = 0
+	GameState.num_floor_for_boss = GameState.num_floor_for_boss + 1
+	
+	current_level = PRELOADS.generation_node.instantiate()
+	level_container.add_child(current_level)
+	
+	current_level.level_finished.connect(_on_level_finished)
+	GameState.enemy_power += 0.1
+	if GameState.num_floor % 3 == 0:
+		GameState.count_rooms += 1
+		GameState.enemies_in_room += 1
+		if GameState.count_rooms > GameState.size_dungeon ** 2:
+			# если подземелье максимального размера(все коматы есть) увеличиваем его размер
+			GameState.size_dungeon += 1
+	
+	move_player_to_spawn(current_level)
+
+
+func load_boss_room():
+	unload_level()
+	
+	
+	if GameState.num_global_level == 0:
+		GameState._enemies_count = 2
+		current_level = PRELOADS.boss_room_1.instantiate()
+	elif GameState.num_global_level == 1:
+		GameState._enemies_count = 1
+		current_level = PRELOADS.boss_room_2.instantiate()
+
+	level_container.add_child(current_level)
+	print("количество врагов - ", GameState._enemies_count)
+	current_level.level_finished.connect(_on_level_finished)
+	move_player_to_spawn(current_level)
+	
+	GameState.levels_for_boss += 1
+	GameState.num_floor_for_boss = 0
+	if GameState.game_mode == GameState.GameMode.STORY:
+		GameState.num_global_level += 1
+	
+	if GameState.num_global_level >= 2: # 2 тк 2 босса пока
+		GameState.game_completed = true
+
+
+func unload_level():
+	if current_level:
+		print("удаляю - ", current_level)
+		current_level.queue_free()
+		current_level = null
+
+
+func move_player_to_spawn(level_scene):
+	# ищем узел SpawnPoint в уровне
+	var spawn = level_scene.get_node("SpawnPoint")
+	if spawn:
+		player_instance.global_position = spawn.global_position
---
+++ тишина-веков/scenes/enemies/boss_skeleton.gd
@@ -0,0 +1,201 @@
+extends "res://сцены/enemies/enemy.gd"
+
+@onready var animSkelet = $AnimationPlayer  # Ссылка на нод анимаций
+@onready var sprite1 = $Sprite2D  # Ссылка на спрайт персонажа
+
+enum State {
+	IDLE, # ожидание, пока игрок не войдёт в радиус агро
+	CHASE, # преследование игрока
+	ATTACK_WINDUP, # подготовка к атаке (телеграф - визульный сигнал)
+	ATTACK, # удар
+	RECOVER, # короткая «перезарядка» после удара
+	REPOSITION # перемещение для удержания оптимальной дистанции (отступление)
+}
+
+var state : State = State.IDLE
+
+@export var aggro_range = 300.0 # дистанция на которой босс тебя замечает
+@export var attack_range = 25.0 # дистанция на которой босс атакует
+@export var windup_time = 0.4 # время на подготовку атаки. типо чтобы игрок успел увернуться
+@export var recover_time = 1.5 # как часто атакует в секундах
+@export var reposition_speed = 180.0 # скорость отступления
+@export var optimal_range = 100.0 # дистанция переключения с отступления на нападение
+
+func _ready():
+	move_speed = 140.0
+	super._ready() # вызываем родительский ready
+
+
+func _physics_process(delta):
+	match state:
+		State.IDLE:
+			print("вошел в состояние idle")
+			state_idle(delta)
+
+		State.CHASE:
+			state_chase(delta)
+			
+
+		State.ATTACK_WINDUP:
+			state_attack_windup(delta)
+
+		State.ATTACK:
+			state_attack(delta)
+
+		State.RECOVER:
+			state_recover(delta)
+
+		State.REPOSITION:
+			state_reposition(delta)
+			
+	#print("вошел в состояние - ")
+	move_and_slide()
+
+
+func state_idle(delta): # работает
+	var player = get_tree().get_first_node_in_group("игрок")
+	if not player:
+		return
+	
+	update_idle_animation()
+
+	if global_position.distance_to(player.global_position) <= aggro_range:
+		print("вошел в состояние chase")
+		state = State.CHASE
+
+
+
+func update_idle_animation():
+	animSkelet.play("покой")
+
+	var player = get_tree().get_first_node_in_group("игрок")
+	if not player:
+		return
+
+	var dir_x = player.global_position.x - global_position.x
+
+	if dir_x != 0:
+		sprite1.scale.x = sign(dir_x)
+
+
+
+var _windup_timer = 0.0
+
+func state_chase(delta):
+	var player = get_tree().get_first_node_in_group("игрок")
+	if not player:
+		return
+
+	var dist = global_position.distance_to(player.global_position)
+
+	# движение на игрока
+	var dir = (player.global_position - global_position).normalized()
+	velocity = dir * move_speed
+	
+	update_move_animation()
+
+	if dist <= attack_range:
+		_windup_timer = windup_time
+		print("ожидание - ", windup_time)
+		print("вошел в состояние attack_windup")
+		state = State.ATTACK_WINDUP
+		
+
+var flag_last_direction = 0
+func update_move_animation():
+	flag_last_direction = 1
+	# Горизонтальное движение - анимация "вид сбоку"
+	animSkelet.play("бег_с_боку")
+	# Разворот спрайта в направлении движения
+	if abs(velocity.x) > 5: # задаем типо чувствительность, то есть будет менять направление при изменении хотя бы 5 пикселей
+		if sign(sprite1.scale.x) != sign(velocity.x):
+			sprite1.scale.x *= -1
+
+
+
+func state_attack_windup(delta):
+	#print(_windup_timer)
+	velocity = Vector2.ZERO
+
+	_windup_timer -= delta
+	if _windup_timer <= 0:
+		#state = State.ATTACK
+		enter_attack()
+
+
+func enter_attack():
+	print("вошел в состояние attack")
+	state = State.ATTACK
+	_attack_timer = attack_duration
+	_attack_finished = false
+
+	velocity = Vector2.ZERO
+	#face_player()
+	animSkelet.play("атака_скелета")
+	
+	var player = get_tree().get_first_node_in_group("игрок")
+	# Этот вызов возвращает первый узел, который состоит в группе "игрок" и проверяет пересекается ли он с хитбоксом
+	if not _damage_applied and $"ОбластьАтаки".overlaps_body(player): 
+		await get_tree().create_timer(0.2).timeout
+		player.take_damage(attack_damage)
+		_damage_applied = true
+
+
+var _attack_finished = false
+var attack_duration = 0.6 # длительность атаки
+var _attack_timer = 0.0
+
+var _damage_applied = false
+func state_attack(delta):
+	velocity = Vector2.ZERO
+	
+	_attack_timer -= delta
+	
+	if _attack_timer <= 0:
+		_attack_finished = true
+	
+	if _attack_finished:
+		_attack_finished = false
+		_damage_applied = false
+		_recover_timer = recover_time
+		#state = State.RECOVER
+		print("вошел в состояние reposition")
+		state = State.REPOSITION
+
+
+var _recover_timer = 0.0
+
+func state_recover(delta):
+	velocity = Vector2.ZERO
+	update_idle_animation()
+	_recover_timer -= delta
+	if _recover_timer > 0:
+		return # если время ожидания не кончилось, ничего не делаем
+
+	var player = get_tree().get_first_node_in_group("игрок")
+	if not player:
+		return
+	
+	var dist = global_position.distance_to(player.global_position)
+	
+	if dist < attack_range:
+		print("вошел в состояние attack_windup")
+		state = State.ATTACK_WINDUP
+	else:
+		print("вошел в состояние chase")
+		state = State.CHASE
+
+
+func state_reposition(delta):
+	var player = get_tree().get_first_node_in_group("игрок")
+	if not player:
+		return
+	update_move_animation()
+
+	var dir = (global_position - player.global_position).normalized()
+	velocity = dir * reposition_speed
+
+	var dist = global_position.distance_to(player.global_position)
+	if dist >= optimal_range:
+		print("вошел в состояние recover")
+		state = State.RECOVER # отбегает и ждет немного перед следующей атакой
